9f349c8758674c6097a27f1f2ab817455b027960
Tam <tamknguyen3@gmail.com>
2024-11-17 01:44:30 -0500

    utils, print tested via dune utop

--- src/lib/utils.ml
+++ src/lib/utils.ml
@@ -1,51 +1,7 @@
-
+open Stdio 
 [@@@ocaml.warning "-27"]
 
-type word_dict = (int * string list) list
-
-(* let read_words (filename : string) : word_dict =
-  let input_channel = filename in
-  let rec read_lines acc =
-      let line = input_channel in 
-      if String.length line > 0 then
-        let process_line line =
-          match String.split_on_chars line ~on:[':'] with
-          | [length_str; words_str] ->
-              let length = int_of_string (length_str) in
-              let words =
-                words_str
-                |> (fun s -> String.sub s 1 (String.length s - 2)) (* remove outer brackets *)
-                |> String.split_on_char ~sep:','                     (* split to individual words *)
-                
-              in
-              (length, words) :: acc
-          | _ -> acc (* skip malformed lines *)
-        in
-        read_lines (process_line line)
-      else
-        read_lines acc (* skip empty lines *)
-
-  in
-    let result = read_lines [] in
-      close_in input_channel;
-      result *)
-
-(* let () =
-  let words = read_words "words.txt" in
-  List.iter (fun (length, word_list) ->
-      Printf.printf "%d: [%s]\n" length (String.concat ", " word_list))
-    words *)
-
-let select_words (word_dict : word_dict) (total_chars : int) : string list =
-  []
-  
-
-
-
-
-
-
-(* hard coded for testing *)
+(* Hard coded for testing front end *)
 let sample_grid : char list list = 
   let grid = 
     [['e'; 'b'; 'l'; 'y'; 'r'; 'r'];
@@ -57,3 +13,34 @@ let sample_grid : char list list =
     ['a'; 'o'; 'e'; 'r'; 'n'; 'a'];
     ['p'; 'p'; 'l'; 'y'; 'i'; 'r']]
   in grid
+
+(* parses a file and returns a list of words *)
+let parse_file (filename : string) : string list = 
+  let content = In_channel.read_all filename in 
+  let words_list = String.split_on_char ' ' content in 
+    List.map String.trim words_list  
+
+(* 
+  in our implementation, spangram is the first word of the words list
+  and the remaining are the themed words 
+*)
+let get_spangram (words : string list) = 
+  match words with 
+  | [] -> "" 
+  | hd :: tl -> hd           
+
+let get_words (words : string list) = 
+  match words with 
+  | [] -> []              
+  | _ :: tl -> tl       
+
+(* print testing, shown in dune utop ! *)
+let () = 
+  let words = parse_file "sample_words.txt" in 
+  let spangram = get_spangram words in 
+    Printf.printf "This is our spangram! %s\n" spangram
+    
+let () =     
+  let words = parse_file "sample_words.txt" in
+  let themed_words = get_words words in
+    List.iter print_endline themed_words
