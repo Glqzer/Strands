d3b971ba70d967baeff21bb27391a3005fea9b65
Mia Choi <miaminchoe@gmail.com>
2024-12-04 14:43:06 -0500

    added headers

--- src/server/server.ml
+++ src/server/server.ml
@@ -9,7 +9,7 @@ let solution_coords =
 let cors_headers = [
   ("Access-Control-Allow-Origin", "*");
   ("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  ("Access-Control-Allow-Headers", "Content-Type");
+  ("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Origin, Access-Control-Allow-Methods");
 ]
 
 let handle_options_request _ = 
