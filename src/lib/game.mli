0070543d20d911458e6f2632949c9e721d265f46
Mia Choi <miaminchoe@gmail.com>
2024-12-10 21:10:32 -0500

    renamed state to config

--- src/lib/game.mli
+++ src/lib/game.mli
@@ -2,21 +2,21 @@ open Grid
 open Words
 
 module Game : sig
-  type state = {
+  type config = {
   board: Grid.t;  
   word_coords: WordCoords.t;  
   word_records: WordRecord.t; 
   spangram : string;
  }
 
-  (** [initialize_game state] initializes the game state *)
-  val initialize_game : state -> state
+  (** [initialize_game config] initializes the game config *)
+  val initialize_game : config -> config
 
-  (** [check_word word coords state] checks if a given word at the specified
+  (** [check_word word coords config] checks if a given word at the specified
     coordinates matches the valid words on the board*)
-  val check_word : string -> (int * int ) list -> state -> bool
+  val check_word : string -> (int * int ) list -> config -> bool
 
   (** [set_found word state] updates the game state to mark the given word as found *)
-  val set_found : string -> state -> state
+  val set_found : string -> config -> config
 
 end
